c1a76bf77a3c539e6807f675e86edb98f76b6944
Fernando Cesar <202507363778@alunos.estacio.br>
2025-10-20 09:50:59 -0300

    Implementa cadastro de cartas com entrada e exibição de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,62 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+#define NUM_CARTAS 3
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+typedef struct {
+    char codigo[4];
+    long long populacao;
+    double area;
+    double pib;
+    int pontos_turisticos;
+} Carta;
 
-  // Área para entrada de dados
+int main(void) {
+    Carta cartas[NUM_CARTAS];
+    int i;
 
-  // Área para exibição dos dados da cidade
+    printf("Cadastro de %d cartas (8 estados x 4 cidades) \n", NUM_CARTAS);
 
-return 0;
-} 
+    for (i = 0; i < NUM_CARTAS; i++) {
+        printf("\nCarta %d de %d\n", i + 1, NUM_CARTAS);
+        printf("Codigo (ex: A01): ");
+        if (scanf("%3s", cartas[i].codigo) != 1) {
+            printf("Erro na leitura do codigo.\n");
+            return 1;
+        }
+
+        printf("Populacao: ");
+        if (scanf("%lld", &cartas[i].populacao) != 1) {
+            printf("Erro na leitura da populacao.\n");
+            return 1;
+        }
+
+        printf("Area: ");
+        if (scanf("%lf", &cartas[i].area) != 1) {
+            printf("Erro na leitura da area.\n");
+            return 1;
+        }
+
+        printf("PIB: ");
+        if (scanf("%lf", &cartas[i].pib) != 1) {
+            printf("Erro na leitura do PIB.\n");
+            return 1;
+        }
+
+        printf("Pontos Turisticos: ");
+        if (scanf("%d", &cartas[i].pontos_turisticos) != 1) {
+            printf("Erro na leitura dos pontos turisticos.\n");
+            return 1;
+        }
+    }
+    printf("\n===Cartas Cadastradas===\n");
+    for (i = 0; i < NUM_CARTAS; i++) {
+        printf("\nCarta %d:\n", i + 1);
+        printf("Codigo: %s\n", cartas[i].codigo);
+        printf("Populacao: %lld\n", cartas[i].populacao);
+        printf("Area: %.2lf\n", cartas[i].area);
+        printf("PIB: %.2lf\n", cartas[i].pib);
+        printf("Pontos Turisticos: %d\n", cartas[i].pontos_turisticos);
+    }
+    return 0;
+}
